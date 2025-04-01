1dd77deb62f0c367fec1f8c738e8c90d5b581391
Johdasgran <dampig14@gmail.com>
2025-03-31 23:28:55 -0500

    next lab

---
+++ lab_2_1.py
@@ -0,0 +1,41 @@
+name = '';
+number = 0;
+valientes = []
+valiente = []
+
+for i in range(0,3):
+    valiente = []
+    name = str(input("Ingrese su nombre valiente del reino: "))
+    number = int(input("Ingrese un numero positivo caballero: "))
+    while (number < 0):
+        print("Alerta! solo los numeros positivos son bienvenidos")
+        number = int(input("Ingrese un numero positivo caballero: "))
+        if(number >= 0):
+            break
+    valiente.append(name)
+    valiente.append(number)
+    valientes.append(valiente)
+
+
+for x in range(len(valientes)):
+    for y in range(0, valientes[x][1]):
+        print(f"{y+1}. {valientes[x][0]}")
+
+    
+print("Los valientes son: ", valientes)
+
+
+binario = 101011
+binario_str = str(binario)[::-1]
+binario_len = len(binario_str)
+decimal = 0;
+
+for r in range(0, binario_len):
+    if(int(binario_str[r]) != 0):
+        decimal += 2**r
+
+
+print(f"El numero binario '{binario}' en decimal es: {decimal}")
+
+
+
